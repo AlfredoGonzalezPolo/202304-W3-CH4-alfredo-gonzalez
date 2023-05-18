a31448c5543fed41372a1646f2f4f50f713eee4c
Alfredo González Polo <fredigonzapolo@hotmail.com>
2023-05-18 18:08:27 +0200

    Add class movie with moviestructure type

---
+++ src/models/movie.ts
@@ -0,0 +1,23 @@
+export type MovieStructure = {
+  id: number;
+  name: string;
+  creator: string;
+  year: number;
+  poster: string;
+  watched: boolean;
+  score: number;
+  emmies: number;
+};
+
+export class Movie implements MovieStructure {
+  constructor(
+    public id: number,
+    public name: string,
+    public creator: string,
+    public year: number,
+    public poster: string,
+    public watched: boolean,
+    public score: number,
+    public emmies: number
+  ) {}
+}
